016ab7c1a6339e0f193de9c41f4767f151cfd11c
Tomer du Sautoy <tomerdusautoy@gmail.com>
2021-11-27 18:49:41 +0000

    add mint button and test contract

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Contract, ethers } from 'ethers'
+import abi from '../smart-contract/abi.json'
 
+const contract = '0xc7dc463dc36196aa725f14950967a01269ea436f'
 
 const Home = () => {
     const provider = new ethers.providers.Web3Provider((window as any).ethereum)
@@ -8,8 +10,20 @@ const Home = () => {
     const connectMetamask = async () => {
         await provider.send("eth_requestAccounts", []);
         const signer = provider.getSigner();
-        console.log("Account:", await signer.getAddress());
+        
+        const nft = new ethers.Contract(contract, abi, signer)
+
+        try {
+            await nft.mint(1, { value: ethers.utils.parseEther('0.0318')})
+            alert('succesful mint')
+        } catch (e: any) {
+            alert(e.message)
+        }
+
+
     }
+
+
 
     return <div>
         <button onClick={() => connectMetamask()}>connect</button>
