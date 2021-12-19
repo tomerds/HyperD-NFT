9dfa382daa85c7224c2f74c8b25663de257160cc
Tomer du Sautoy <tomerdusautoy@gmail.com>
2021-12-19 19:57:24 +0000

    tidy up

--- smart-contracts/scripts/deploy.js
+++ smart-contracts/scripts/deploy.js
@@ -5,7 +5,7 @@ async function main() {
 
   console.log('Account balance:', (await deployer.getBalance()).toString());
 
-  const Token = await ethers.getContractFactory('Token');
+  const Token = await ethers.getContractFactory('HyperDimensionalNft');
   const token = await Token.deploy();
 
   console.log('Token address:', token.address);
